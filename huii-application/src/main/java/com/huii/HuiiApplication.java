d0fe2777a4f6cd2a98347ebc3b7a201469a6eed4
huii <1659009445@qq.com>
2023-11-06 20:18:12 +0800

    init project

---
+++ huii-application/src/main/java/com/huii/HuiiApplication.java
@@ -0,0 +1,19 @@
+package com.huii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuiiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuiiApplication.class, args);
+        System.out.println("HUII-APPLICATION-START-SUCCESS");
+        System.out.println("-------------------------------");
+        System.out.println("----------　／l、---------------");
+        System.out.println("----------（°､ 。 ７------------");
+        System.out.println("---------- l、 ~ヽ--------------");
+        System.out.println("---------- じしf_, )ノ----------");
+        System.out.println("-------------------------------");
+    }
+}
