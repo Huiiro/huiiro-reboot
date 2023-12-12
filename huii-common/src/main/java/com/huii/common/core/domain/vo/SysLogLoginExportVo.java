fa844565ecb0a4b931244f0a7652ca8ff7586ed7
huii <1659009445@qq.com>
2023-12-12 08:52:43 +0800

    complete some frontend ... and deal with logs

--- huii-common/src/main/java/com/huii/common/core/domain/vo/SysLogLoginExportVo.java
+++ huii-common/src/main/java/com/huii/common/core/domain/vo/SysLogLoginExportVo.java
@@ -1,4 +1,51 @@
 package com.huii.common.core.domain.vo;
 
-public class SysLogLoginExportVo {
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.huii.common.annotation.ExcelData;
+import com.huii.common.convert.ExcelDataConvert;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+public class SysLogLoginExportVo  implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @ExcelProperty(value = "登录日志ID")
+    private Long loginId;
+
+    @ExcelProperty(value = "用户名称")
+    private String loginUserName;
+
+    @ExcelProperty(value = "登录IP")
+    private String loginIp;
+
+    @ExcelProperty(value = "登录地点")
+    private String loginAddress;
+
+    @ExcelProperty(value = "登录时间")
+    private LocalDateTime loginTime;
+
+    @ExcelProperty(value = "浏览器")
+    private String loginBrowser;
+
+    @ExcelProperty(value = "操作系统")
+    private String loginOs;
+
+    @ExcelProperty(value = "登录方式", converter = ExcelDataConvert.class)
+    @ExcelData(readConverterExp = "1=账号登录,2=邮箱登录,3=手机登录,4=Github登录,5=Gitee登录")
+    private Integer loginType;
+
+    @ExcelProperty(value = "登陆结果", converter = ExcelDataConvert.class)
+    @ExcelData(readConverterExp = "0=失败,1=成功")
+    private String loginStatus;
+
+    @ExcelProperty(value = "登录信息")
+    private String loginMessage;
 }
--- huii-common/src/main/java/com/huii/common/core/domain/vo/SysLogOpExportVo.java
+++ huii-common/src/main/java/com/huii/common/core/domain/vo/SysLogOpExportVo.java
@@ -1,4 +1,68 @@
 package com.huii.common.core.domain.vo;
 
-public class SysLogOpExportVo {
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.huii.common.annotation.ExcelData;
+import com.huii.common.convert.ExcelDataConvert;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+public class SysLogOpExportVo implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @ExcelProperty(value = "操作日志ID")
+    private Long opId;
+
+    @ExcelProperty(value = "操作用户名")
+    private String opUserName;
+
+    @ExcelProperty(value = "请求方法")
+    private String opMethodName;
+
+    @ExcelProperty(value = "操作类型", converter = ExcelDataConvert.class)
+    @ExcelData(readConverterExp = "1=其他,2=查询,3=授权," +
+            "4=新增,5=修改,6=删除,7=导入,8=导出,9=生成,10=清空")
+    private Integer opType;
+
+    @ExcelProperty(value = "请求时间")
+    private LocalDateTime opTime;
+
+    @ExcelProperty(value = "接口耗时(ms)")
+    private String opCostTime;
+
+    @ExcelProperty(value = "请求IP")
+    private String opIp;
+
+    @ExcelProperty(value = "操作地点")
+    private String opAddress;
+
+    @ExcelProperty(value = "请求方式")
+    private String opRequest;
+
+    @ExcelProperty(value = "请求地址")
+    private String opUrl;
+
+    @ExcelProperty(value = "请求参数")
+    private String opReqParam;
+
+    @ExcelProperty(value = "响应参数")
+    private String opResParam;
+
+    @ExcelProperty(value = "请求结果", converter = ExcelDataConvert.class)
+    @ExcelData(readConverterExp = "0=失败,1=成功")
+    private String opStatus;
+
+    @ExcelProperty(value = "标记请求", converter = ExcelDataConvert.class)
+    @ExcelData(readConverterExp = "0=未标记,1=标记")
+    private String opMarkFlag;
+
+    @ExcelProperty(value = "请求结果信息")
+    private String opMessage;
 }
