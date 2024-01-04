792a9834f5a268de418e530a269f9c214d007b63
huii <1659009445@qq.com>
2024-01-04 23:46:12 +0800

    ds

--- huii-common/src/main/java/com/huii/common/annotation/DataSource.java
+++ huii-common/src/main/java/com/huii/common/annotation/DataSource.java
@@ -1,5 +1,7 @@
 package com.huii.common.annotation;
 
+import com.huii.common.enums.DynamicDataSourceType;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -11,6 +13,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface DataSource {
-
-
+    DynamicDataSourceType value() default DynamicDataSourceType.MASTER;
 }
---
+++ huii-common/src/main/java/com/huii/common/enums/DynamicDataSourceType.java
@@ -0,0 +1,19 @@
+package com.huii.common.enums;
+
+/**
+ * 动态数据源
+ *
+ * @author huii
+ */
+public enum DynamicDataSourceType {
+
+    /**
+     * 主库
+     */
+    MASTER,
+
+    /**
+     * 从库
+     */
+    SLAVE;
+}
