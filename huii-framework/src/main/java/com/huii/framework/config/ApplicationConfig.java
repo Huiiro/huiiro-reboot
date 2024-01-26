ee00df88751a0521768f878607bad033ea72df8c
huii <1659009445@qq.com>
2024-01-26 23:20:59 +0800

    fix bug

--- huii-framework/src/main/java/com/huii/framework/config/ApplicationConfig.java
+++ huii-framework/src/main/java/com/huii/framework/config/ApplicationConfig.java
@@ -1,5 +1,7 @@
 package com.huii.framework.config;
 
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
@@ -11,4 +13,12 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @Configuration
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 public class ApplicationConfig {
+
+    /**
+     * 临时解决springboot3.x(tomcat core 10.x)以上版本 async task request对象回收问题
+     */
+    @Bean
+    TomcatConnectorCustomizer disableFacadeDiscard() {
+        return (connector) -> connector.setDiscardFacades(false);
+    }
 }
